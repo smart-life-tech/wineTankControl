8f98ec5d7fcd5fda97b67c52e69c5bbc18abdf4b
smart-life-tech <christlightworld@gmail.com>
2023-09-25 14:46:39 +0100

    modified:   wineTankControl.ino

--- wineTankControl.ino
+++ wineTankControl.ino
@@ -3,8 +3,8 @@
 #include <EasyNextionLibrary.h>
 #include <EEPROM.h>    // knihovna pro zapis do pameti eeprom
 #define ONE_WIRE_BUS 4 // The one-wire bus pin
-#define RELAY_PINS                 \
-  {                              \
+#define RELAY_PINS             \
+  {                            \
     23, 1, 3, 19, 25, 18, 5, 0 \
   } // Example2 relay pins
 
@@ -19,17 +19,17 @@ float currentTankTemperatures[numRelays]; // Array to store current temperatures
 
 EasyNex myNex(Serial2); // Use the correct Serial port and baud rate
 // Adresy cidel
-DeviceAddress tank1, tank2, tank3, tank4, tank5, tank6, tank7, tank8;
-/*
-  DeviceAddress tank1 = {0x28, 0xFF, 0xE0, 0x19, 0x2, 0x17, 0x4, 0x95}; // 28 FF E0 19 2 17 4 95
-  DeviceAddress tank2 = {0x28, 0xFF, 0x2A, 0xA6, 0x1, 0x17, 0x4, 0xE};  // 28 FF 2A A6 1 17 4 E
-  DeviceAddress tank3 = {0x28, 0x1F, 0xBD, 0xCB, 0x04, 0x00, 0x00, 0xF1};
-  DeviceAddress tank4 = {0x28, 0x60, 0x67, 0xCE, 0x04, 0x00, 0x00, 0x2F};
-  DeviceAddress tank5 = {0x28, 0x1F, 0xBD, 0xCB, 0x04, 0x00, 0x00, 0xF1};
-  DeviceAddress tank6 = {0x28, 0x60, 0x67, 0xCE, 0x04, 0x00, 0x00, 0x2F};
-  DeviceAddress tank7 = {0x28, 0x1F, 0xBD, 0xCB, 0x04, 0x00, 0x00, 0xF1};
-  DeviceAddress tank8 = {0x28, 0x60, 0x67, 0xCE, 0x04, 0x00, 0x00, 0x2F};
-*/
+// DeviceAddress tank1, tank2, tank3, tank4, tank5, tank6, tank7, tank8;
+
+DeviceAddress tank1 = {0x28, 0xFF, 0xE0, 0x19, 0x2, 0x17, 0x4, 0x95};   // 28 FF E0 19 2 17 4 95
+DeviceAddress tank2 = {0x28, 0x58, 0x6, 0x80, 0xE3, 0xE1, 0x3D, 0x5};   // 28 58 6 80 E3 E1 3D 5
+DeviceAddress tank3 = {0x28, 0xA, 0xAE, 0x80, 0xE3, 0xE1, 0x3D, 0x48};  // 28 A AE 80 E3 E1 3D 48
+DeviceAddress tank4 = {0x28, 0x47, 0xBF, 0x80, 0xE3, 0xE1, 0x3D, 0xC6}; // 28 47 BF 80 E3 E1 3D C6
+DeviceAddress tank5 = {0x28, 0xCD, 0xF2, 0x80, 0xE3, 0xE1, 0x3D, 0xE6}; // 28 CD F2 80 E3 E1 3D E6
+DeviceAddress tank6 = {0x28, 0xB6, 0xA5, 0x43, 0xD4, 0xE1, 0x3D, 0x57}; // 28 B6 A5 43 D4 E1 3D 57
+DeviceAddress tank7 = {0x28, 0x71, 0xBB, 0x80, 0xE3, 0xE1, 0x3D, 0x86}; // 28 71 BB 80 E3 E1 3D 86
+DeviceAddress tank8 = {0x28, 0x68, 0x7E, 0x43, 0xD4, 0xE1, 0x3D, 0xAE}; // 28 68 7E 43 D4 E1 3D AE
+
 // promenne teploty
 unsigned long casCteniTeplot; // cas posledniho cteni
 boolean povolCteniTeplot = false;
@@ -40,45 +40,19 @@ byte resolutionDS = 12;                   // nastaveni rozliseni cidla
 // pole adres cidel DS 18B20 - 8 pozic dalsi volna adresa 624
 // const int cidlaDS_i[] = {560, 568, 576, 584, 592, 600, 608, 616};
 const int cidlaDS_i = 560; // prvni adresa EEPROM pro zapis adres cidel
-void hledaniDS18B20()
-{
-  byte address[8];
-  byte id;
-  oneWire.reset_search(); // resetuj hledani
-  if (oneWire.search(address))
-  { // hledej adresy
-    for (id = 0; id < numberOfSensors; id++)
-    { // zapis data do displeje
-      String stringText;
-      for (byte i = 0; i < 8; i++)
-      { // precti a uloz adresy
-        EEPROM.write(cidlaDS_i + (id * 8) + i, address[i]);
-        stringText += address[i], DEC;
-        if (i < 8 - 1)
-          stringText += ",";
-      }
-      Serial.println("Cidlo " + (String)(id + 1) + ": " + stringText);
-      if (!oneWire.search(address))
-      {
-        oneWire.reset_search();
-        Serial.println(F("Zadne dalsi cidlo nenalezeno"));
-        break;
-      }
-    }
-    Serial.println("Nalezeno " + (String)(id + 1) + " cidel.");
-    Serial.println();
-  }
-}
+
 void printAddress(DeviceAddress deviceAddress)
 {
   for (uint8_t i = 0; i < 8; i++)
   {
     // zero pad the address if necessary
-    if (deviceAddress[i] < 16) Serial.print("0");
+    if (deviceAddress[i] < 16)
+      Serial.print("0");
     Serial.print(deviceAddress[i], HEX);
   }
 }
-void printAll() {
+void printAll()
+{
   Serial.print("Device 0 Address: ");
   printAddress(tank1);
   Serial.println();
@@ -103,7 +77,6 @@ void printAll() {
   Serial.print("Device 7 Address: ");
   printAddress(tank8);
   Serial.println();
-
 }
 void setup()
 {
@@ -122,27 +95,28 @@ void setup()
     Serial.println("ON");
   else
     Serial.println("OFF");
+  /*
+    oneWire.reset_search();
+    //assigns the first address found to tank1
+    if (!oneWire.search(tank1)) Serial.println("Unable to find address for tank1");
+    //assigns the seconds address found to tank2
+    if (!oneWire.search(tank2)) Serial.println("Unable to find address for tank2");
+    //assigns the first address found to tank1
+    if (!oneWire.search(tank3)) Serial.println("Unable to find address for tank3");
+    //assigns the seconds address found to tank2
+    if (!oneWire.search(tank4)) Serial.println("Unable to find address for tank4");
+    //assigns the first address found to tank1
+    if (!oneWire.search(tank5)) Serial.println("Unable to find address for tank5");
+    //assigns the seconds address found to tank2
+    if (!oneWire.search(tank6)) Serial.println("Unable to find address for tank6");
+    //assigns the first address found to tank1
+    if (!oneWire.search(tank7)) Serial.println("Unable to find address for tank7");
+    //assigns the seconds address found to tank2
+    if (!oneWire.search(tank8)) Serial.println("Unable to find address for tank8");
+    */
 
-  oneWire.reset_search();
-  //assigns the first address found to tank1
-  if (!oneWire.search(tank1)) Serial.println("Unable to find address for tank1");
-  //assigns the seconds address found to tank2
-  if (!oneWire.search(tank2)) Serial.println("Unable to find address for tank2");
-  //assigns the first address found to tank1
-  if (!oneWire.search(tank3)) Serial.println("Unable to find address for tank3");
-  //assigns the seconds address found to tank2
-  if (!oneWire.search(tank4)) Serial.println("Unable to find address for tank4");
-  //assigns the first address found to tank1
-  if (!oneWire.search(tank5)) Serial.println("Unable to find address for tank5");
-  //assigns the seconds address found to tank2
-  if (!oneWire.search(tank6)) Serial.println("Unable to find address for tank6");
-  //assigns the first address found to tank1
-  if (!oneWire.search(tank7)) Serial.println("Unable to find address for tank7");
-  //assigns the seconds address found to tank2
-  if (!oneWire.search(tank8)) Serial.println("Unable to find address for tank8");
-  printAll();
-  //hledaniDS18B20();
-  // method 1: by index
+  // hledaniDS18B20();
+  //  method 1: by index
   if (!sensors.getAddress(tank1, 0))
     Serial.println("Unable to find address for Device 0");
   if (!sensors.getAddress(tank2, 1))
@@ -159,7 +133,7 @@ void setup()
     Serial.println("Unable to find address for Device 6");
   if (!sensors.getAddress(tank8, 7))
     Serial.println("Unable to find address for Device 7");
-
+  printAll();
   for (int i = 0; i < numRelays; i++)
   {
     pinMode(relayPins[i], OUTPUT);
